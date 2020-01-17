dadf3b60dde25c32bf3358156890e00d557aafed
garciajnatalie <37346907+garciajnatalie@users.noreply.github.com>
2020-01-16 21:04:52 -0500

    form created, javascript started

---
+++ js/index.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+//create Form
+//insert text into form
+//find submit Button
+//when submitted, create task when submitted
+//add text to innerHTML
+// Create an array of pre-existing todos that will be added to the page on page load  by looping through them one at a time (see my application has “Do laundry” and  “Cook Dinner” 
+//Bonus - Try to add a remove button that destroys the list item 
+//Bonus 2 - Begin styling your todo list 
+
+  var form = document.querySelector('form');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log('form submitted');
+
+  });
+
+
+
+
+
+
+
+
+
+
+});
